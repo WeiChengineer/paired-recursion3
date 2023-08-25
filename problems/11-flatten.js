64a5e33275f59c951b98dbcee0fe3cf285a9df53
Ortland Meadows <strategyandopportunity@gmail.com>
2023-08-24 22:01:37 -0500

    11-is has beens solvedish

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -5,16 +5,23 @@ contents on one level.
 
 Do not use the built in Array.flat.
 
-Examples:
 
-flatten([]); // []
-flatten([1, 2]); // [1, 2]
-flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here 
+  if (arr.length === 0) return [] // (!arr.length)
+  if (Array.isArray(arr[0])) {
+    return flatten(arr[0]).concat(flatten(arr.slice(1))) 
+  } else {
+   return [arr[0]].concat(flatten(arr.slice(1)))
+  } 
 }
+
+// Examples:
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
   
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
